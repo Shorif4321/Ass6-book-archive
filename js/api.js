7e472283e81aed9951b8a4faaba11ecb33b50eae
Shorif4321 <shorifali4321@gmail.com>
2021-09-02 02:54:34 +0600

    new comit

--- js/api.js
+++ js/api.js
@@ -5,16 +5,29 @@ const loadData = () => {
     const searchText = getInput.value;
     getInput.value = '';
 
+
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayData(data))
-}
+
+    const errorMassage = document.getElementById('error');
+    if (searchText === '') {
+        errorMassage.textContent = '';
+        const p = document.createElement('p');
+        p.innerHTML = `<h2 class="text-center text-danger">Please write something<h2>`;
+        errorMassage.appendChild(p);
+    }
+    else {
+        errorMassage.textContent = '';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayData(data))
+    }
+};
 
 const displayData = allBooks => {
     const books = allBooks.docs;
     const myCustomDiv = document.getElementById('boot-card')
     myCustomDiv.innerText = '';
+
     books.forEach(book => {
         //console.log(book)
         const div = document.createElement('div');
@@ -25,7 +38,7 @@ const displayData = allBooks => {
 
                     <div class="card-body">
                         <h3 class="card-title"> ${book.title} </h3>
-                    <h6 class="card-title">Author: ${book.author_name} </h6>
+                    <h6 class="card-title">Author: ${book.author_name ? book.author_name : 'Here is no author-name'} </h6>
                     <p> First Publish Year : ${book.first_publish_year}</p>
                     <p> Publisher : ${book.publisher}</p>
                     </div>
