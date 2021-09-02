c73558aa857ea68605d724078656b8f001efab5b
Shorif4321 <shorifali4321@gmail.com>
2021-09-02 16:12:11 +0600

    netlifysolve

--- js/api.js
+++ js/api.js
@@ -15,7 +15,7 @@ const loadData = () => {
         return;
     }
     getInput.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayData(data))
